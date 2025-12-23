9e54d1ba2935336f3aea92b0302762664fe3e948
mynameisVictoria <bemil1226@gmail.com>
2025-12-23 14:19:39 +0100

    Add TCP client implementation in main.py
    Implement a basic TCP client that connects to a server, sends a message, and receives a response.

--- src/client/main.py
+++ src/client/main.py
@@ -1,1 +1,34 @@
 
+import socket
+import errno
+
+#-----------------CLIENT-------------------------#
+my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+port = 1111
+
+
+def main():
+    connected = False
+
+    while True:
+
+        if not connected:
+            try:
+                my_socket.connect(("localhost", port))
+                print("socket connected")
+                connected = True
+            except OSError as e:
+                if e.errno in (errno.EISCONN, 56):
+                    print("ERROR")
+                    connected = True
+                else:
+                    connected = False
+
+        elif connected:
+            my_socket.sendall(b"test from client")
+            print(my_socket.recv(1024))
+
+
+main()
